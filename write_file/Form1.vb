3e7c5a8a1da1b1447dc9e47fa0032647698fea74
chrisjoohn <chrisjohnmulingbayan@gmail.com>
2020-02-02 11:05:22 +0800

    initial commit

---
+++ write_file/Form1.vb
@@ -0,0 +1,75 @@
+﻿Imports Newtonsoft.Json
+Public Class Form1
+    Dim name_input As String
+    Dim age As String
+    Dim address As String
+
+    Dim file As System.IO.StreamWriter
+
+
+    Private Sub TextBox2_keyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles TextBox2.KeyPress
+        If Asc(e.KeyChar) <> 8 Then
+            If Asc(e.KeyChar) < 48 Or Asc(e.KeyChar) > 57 Then
+                e.Handled = True
+            End If
+        End If
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        file = My.Computer.FileSystem.OpenTextFileWriter("C:\Users\CJMulingbayan\Documents\Visual Studio 2015\Projects\write_file\write_file\exported_files\" + name_input + ".txt", True)
+        name_input = TextBox1.Text
+        age = TextBox2.Text
+        address = TextBox3.Text
+
+        file.WriteLine("name: " + name_input)
+        file.WriteLine("age: " + age)
+        file.WriteLine("address: " + address)
+
+        file.Close()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        name_input = TextBox1.Text
+        age = TextBox2.Text
+        address = TextBox3.Text
+
+        Dim xml_document As XDocument = <?xml version="1.0" encoding="UTF-8"?>
+                                        <Person>
+                                            <name>
+                                                <%= name_input %>
+                                            </name>
+                                            <age>
+                                                <%= age %>
+                                            </age>
+                                            <address>
+                                                <%= address %>
+                                            </address>
+                                        </Person>
+
+        xml_document.Save("C:\Users\CJMulingbayan\Documents\Visual Studio 2015\Projects\write_file\write_file\exported_files\" + name_input + ".xml")
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        name_input = TextBox1.Text
+        age = TextBox2.Text
+        address = TextBox3.Text
+
+        Dim person As New Person()
+        person.name = name_input
+        person.age = age
+        person.address = address
+
+        Dim output As String = JsonConvert.SerializeObject(person)
+        file = My.Computer.FileSystem.OpenTextFileWriter("C:\Users\CJMulingbayan\Documents\Visual Studio 2015\Projects\write_file\write_file\exported_files\" + name_input + ".json", True)
+        file.WriteLine(output)
+        file.Close()
+    End Sub
+End Class
---
+++ write_file/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("write_file")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("write_file")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("3ab34ace-0a46-4535-b1cc-8bd0dd52a5e3")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
